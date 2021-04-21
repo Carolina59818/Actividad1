e9a82e305d0ec0e50f6c4f631170302dbcf087b4
caroc <caroc@192.168.1.71>
2021-04-20 20:03:24 -0500

    Actividad 1

---
+++ src/actividad1/Actividad1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad1;
+
+/**
+ *
+ * @author caroc
+ */
+public class Actividad1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Escriba un programa que con base al año actual y lea un año cualquiera
+        //y que escriba cuántos años han pasado desde ese año o cuántos años faltan para llegar a ese año
+        
+    }
+    
+}
